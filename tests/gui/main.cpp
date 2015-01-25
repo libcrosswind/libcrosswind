267261f98a1ddaee15a8a574d3b1894d56e3530b
seasidekai <lvictor_9@hotmail.com>
2015-01-25 02:22:14 -0700

    Lambda closure test

--- tests/gui/main.cpp
+++ tests/gui/main.cpp
@@ -18,15 +18,25 @@ int main(int argc, char **argv) {
 
     std::cout<<cw::filesystem::get_file_path("click2.ogg")<<std::endl;
     std::shared_ptr<cw::window> window(new cw::window(640, 480, "GUI test"));
-
     std::shared_ptr<cw::button> button(new cw::button(true));
+
     //button->draggable() = true;
     std::shared_ptr<cw::widget> button_replica1 = button->replicate<cw::button>();
     std::shared_ptr<cw::widget> button_replica2 = button_replica1->replicate<cw::button>();
 
-    button->on_mouse_down += [](int button){
-      std::cout<<button<<std::endl;
-    };
+    button->on_mouse_down += [](){
+
+        class data{
+        public:
+            data():num(10){}
+            int num;
+        };
+
+        std::shared_ptr<data> number_data(new data());
+
+        return [number_data](int number){       std::cout<<number_data->num<<std::endl; };
+
+    }();
 
     window->attach(button);
     window->attach(button_replica1);
