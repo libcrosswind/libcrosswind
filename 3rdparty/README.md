a9507a281f9ff44500edae6c209fe8e8292ffdea
seasidekai <lvictor_9@hotmail.com>
2015-02-27 20:12:01 -0700

    Not using SMPEG

--- 3rdparty/README.md
+++ 3rdparty/README.md
@@ -12,3 +12,22 @@ http://sourceforge.net/projects/tdm-gcc/files/TDM-GCC%20Installer/tdm64-gcc-4.9.
 
 Download, install and ensure it is added to PATH
 (recommended installation dir is platform/windows/compilers/x64/TDM-GCC-64)
+
+Upgrade bash for MSYS
+
+By default we'll this version since it can be integrated safely into MSYS:
+
+http://sourceforge.net/projects/mingw/files/MSYS/Base/bash/bash-3.1.23-1/bash-3.1.23-1-msys-1.0.18-bin.tar.xz/download
+
+The dependencies for upgrading bash are:
+
+http://sourceforge.net/projects/mingw/files/MSYS/Base/regex/regex-1.20090805-2/libregex-1.20090805-2-msys-1.0.13-dll-1.tar.lzma/download
+and
+http://sourceforge.net/projects/mingw/files/MSYS/Base/termcap/termcap-0.20050421_1-2/libtermcap-0.20050421_1-2-msys-1.0.13-dll-0.tar.lzma/download
+
+You can extract the content from the lzma files with tar --lzma -xvf file.tar.lzma
+
+tar --lzma -xvf libregex-1.20090805-2-msys-1.0.13-dll-1.tar.lzma
+tar --lzma -xvf libtermcap-0.20050421_1-2-msys-1.0.13-dll-0.tar.lzma
+
+Once you downloaded and extracted the packages copy the content from the extracted folders folder into their respective place in platform/windows/support/msys/1.0 (or your specified MSYS path) and replace the files when prompted.
--- setup/sdl2.sh
+++ setup/sdl2.sh
@@ -15,15 +15,19 @@ SDL2_IMAGE_DIR_NAME=SDL_image-2
 SDL_IMAGE=$PWD/../external/sdl2/$SDL2_IMAGE_DIR_NAME
 SDL_IMAGE_TEMP=$TEMP_DIR/sdlimage_build
 
-
 ###########################SDL_TTF#################################
 SDL_TTF_DIR_NAME=SDL_ttf-2
 SDL_TTF=$PWD/../external/sdl2/$SDL_TTF_DIR_NAME
 SDL_TTF_TEMP=$TEMP_DIR/sdlttf_build
 
+###########################SDL_MIXER###############################
+SDL_MIXER_DIR_NAME=SDL_mixer-2
+SDL_MIXER=$PWD/../external/sdl2/$SDL_MIXER_DIR_NAME
+SDL_MIXER_TEMP=$TEMP_DIR/sdlmixer_build
 
 
 ###########################Aditional###############################
+#########################IMAGE#######################################
 ZLIB_DIR_NAME=zlib-1.2.8
 ZLIB=$SDL_IMAGE/external/$ZLIB_DIR_NAME
 ZLIB_TEMP=$TEMP_DIR/zlib_build
@@ -42,9 +46,23 @@ PNG_TEMP=$TEMP_DIR/phg_build
 #WEBP=$SDL_IMAGE/external/libwebp-0.3.0
 #WEBP_TEMP=$TEMP_DIR/webp_build
 
+#########################TTF#######################################
 FREETYPE_DIR_NAME=freetype-2.5.5
 FREETYPE=$SDL_TTF/external/$FREETYPE_DIR_NAME
 FREETYPE_TEMP=$TEMP_DIR/freetype_build
+
+#########################MIXER#####################################
+OGG_DIR_NAME=libogg-1.3.1
+OGG=$SDL_MIXER/external/$OGG_DIR_NAME
+OGG_TEMP=$TEMP_DIR/ogg_build
+
+VORBIS_DIR_NAME=libvorbis-1.3.3
+VORBIS=$SDL_MIXER/external/$VORBIS_DIR_NAME
+VORBIS_TEMP=$TEMP_DIR/vorbis_build
+
+SMPEG_DIR_NAME=smpeg2-2.0.0
+SMPEG=$SDL_MIXER/external/$SMPEG_DIR_NAME
+SMPEG_TEMP=$TEMP_DIR/smpeg_build
 
 
 ###########################Default##################################
@@ -57,12 +75,19 @@ mkdir -p $TEMP_DIR
 mkdir -p $SDL2_TEMP
 mkdir -p $SDL_IMAGE_TEMP
 mkdir -p $SDL_TTF_TEMP
+mkdir -p $SDL_MIXER_TEMP
+
 mkdir -p $ZLIB_TEMP
 mkdir -p $JPG_TEMP
 mkdir -p $PNG_TEMP
 #mkdir -p $TIF_TEMP
 #mkdir -p $WEBP_TEMP
+
 mkdir -p $FREETYPE_TEMP
+
+mkdir -p $OGG_TEMP
+mkdir -p $VORBIS_TEMP
+mkdir -p $SMPEG_TEMP
 
 
 ###########################BUILD####################################
@@ -139,6 +164,56 @@ pushd $SDL_TTF_DIR_NAME
 sh ./configure  --disable-sdltest --disable-shared FREETYPE_CONFIG=$INSTALL_DIR/bin/freetype-config --prefix=$INSTALL_DIR LDFLAGS=-L$INSTALL_DIR/lib CPPFLAGS=-I$INSTALL_DIR/include 
 make clean
 make
+make install
+popd
+popd
+
+pushd $OGG_TEMP
+cp -rp $OGG .
+pushd $OGG_DIR_NAME
+sh ./configure  --disable-shared --prefix=$INSTALL_DIR
+make clean
+make 
+make install
+popd
+popd
+
+pushd $VORBIS_TEMP
+cp -rp $VORBIS .
+pushd $VORBIS_DIR_NAME
+sh ./configure  --disable-shared --prefix=$INSTALL_DIR
+make clean
+make 
+make install
+popd
+popd
+
+#Not using smpeg
+#pushd $SMPEG_TEMP
+#cp -rp $SMPEG .
+#pushd $SMPEG_DIR_NAME
+#sh ./configure --disable-shared --prefix=$INSTALL_DIR 
+
+#Patching SMPEG dependencies
+#sed 's/-luuid/-luuid -lstdc++/g' ./libsmpeg2.la > ./libsmpeg2.la.new 
+
+#rm ./libsmpeg2.la
+#mv ./libsmpeg2.la.new ./libsmpeg2.la
+####################
+
+#make clean
+#make 
+#make install
+#popd
+#popd
+
+
+pushd $SDL_MIXER_TEMP
+cp -rp $SDL_MIXER .
+pushd $SDL_MIXER_DIR_NAME
+sh ./configure  --disable-sdltest --disable-shared SMPEG_CONFIG=$INSTALL_DIR/bin/smpeg2-config --prefix=$INSTALL_DIR LDFLAGS=-L$INSTALL_DIR/lib CPPFLAGS=-I$INSTALL_DIR/include 
+make clean
+make 
 make install
 popd
 popd
