5203a951d3c02e7383a46fc2da17869fdf00a6dc
seasidekai <lvictor_9@hotmail.com>
2015-03-01 19:39:16 -0700

    Updated resources

--- tests/gui/main.cpp
+++ tests/gui/main.cpp
@@ -7,14 +7,13 @@
 int main(int argc, char **argv) {
     cw::platform::filesystem::add_directory("assets", true);
 
-    cw::platform::application app;
 
-    app.init();
+    auto app = std::make_shared<cw::platform::application>();
+    app->init();
 
     class dummy_stage: public cw::simulation::stage{
     public:
         dummy_stage(std::shared_ptr<cw::platform::sdl::sdl_renderer> sdl_renderer){
-
 
             cw::geometry::point<int> pos(10, 10);
             cw::geometry::point<int> dim(150, 40);
@@ -29,13 +28,12 @@ int main(int argc, char **argv) {
 
             add(std::make_shared<cw::simulation::standard_image>(pos, dim, sdl_renderer, "sonic.json"));
 
-
         }
     };
 
-    app.stages("current", std::make_shared<dummy_stage>(app.sdl_renderer));
+    app->stages("current", std::make_shared<dummy_stage>(app->sdl_renderer));
 
-    app.run();
+    app->run();
 
     return 0;
 
