248d4374aba59772a2817bbfbab3eb730835a233
seasidekai <lvictor_9@hotmail.com>
2015-03-31 02:50:26 -0700

    Updated resources

--- tests/sonic_the_hedgehog/items/monitor.hpp
+++ tests/sonic_the_hedgehog/items/monitor.hpp
@@ -24,14 +24,14 @@ public:
 	virtual void init(){
 
 		this->add_model("monitor",
-			            glm::vec3(0.0f, 0.0f, 0.0f), 
+			            glm::vec3(40.0f, 0.0f, 0.0f),
 			            glm::vec3(30.0f, 30.0f, 0.0f), "monitors.json");
 
 		this->add_rigid_body("monitor",
-							 glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(30.0f, 30.0f, 5.0f), 1.0f);
+							 glm::vec3(40.0f, 0.0f, 0.0f), glm::vec3(30.0f, 30.0f, 5.0f), 1.0f);
 
 		this->get_rigid_body("monitor")->set_activation_policy(DISABLE_DEACTIVATION);
-		this->get_rigid_body("monitor")->set_linear_factor(glm::vec3(1.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+		this->get_rigid_body("monitor")->set_linear_factor(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 
 
 		this->conditions["broken"] = false;
@@ -42,8 +42,13 @@ public:
 	}
 
 	void destroy(){
+
 		this->conditions["broken"] = true;
+		this->get_model("monitor")->change_animation("monitor_broken");
+
+		map_positions();
 		this->remove_rigid_body("monitor");
+
 	}
 
 	void set_content(const std::string& content){
@@ -52,22 +57,19 @@ public:
 
 	void map_positions(){
 
-		if(!this->conditions["broken"]){
-			glm::vec3 b_origin(this->get_rigid_body("monitor")->get_origin().x,
-					this->get_rigid_body("monitor")->get_origin().y,
-					this->get_rigid_body("monitor")->get_origin().z);
-			this->get_model("monitor")->get_render_sprite_list()["current"]->set_origin(b_origin);
-		}
+		glm::vec3 b_origin(this->get_rigid_body("monitor")->get_origin().x,
+				this->get_rigid_body("monitor")->get_origin().y,
+				this->get_rigid_body("monitor")->get_origin().z);
+		this->get_model("monitor")->get_render_sprite_list()["current"]->set_origin(b_origin);
 
 	}
 
 	virtual void logic(const float& dt){
 
-		if(this->conditions["broken"]){
-			this->get_model("monitor")->change_animation("monitor_broken");
-		}
+		if(!this->conditions["broken"]) {
+			map_positions();
 
-		map_positions();
+		}
 
 	}
 
