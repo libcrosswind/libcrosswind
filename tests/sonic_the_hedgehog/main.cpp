93e72fdb012f79e80b2809907d6bfbfd7a60c25f
seasidekai <lvictor_9@hotmail.com>
2015-03-31 12:50:19 -0700

    Added title schema

--- tests/sonic_the_hedgehog/main.cpp
+++ tests/sonic_the_hedgehog/main.cpp
@@ -1,22 +1,22 @@
 #include <crosswind/engine.hpp>
 
-//#include <scenes/title.hpp>
-#include <scenes/green_hill_zone.hpp>
+#include <scenes/title.hpp>
+//#include <scenes/green_hill_zone.hpp>
 
 int main(int argc, char **argv) {
 
     auto engine = std::make_shared<cw::engine>();
     engine->core->filesystem->add_directory("assets", true);
 
-//    auto title = engine->stage->create_scene<game::scenes::title>();
+    auto title = engine->stage->create_scene<game::scenes::title>();
 
-//    engine->stage->add_scene("title", title);
-//    engine->stage->load_scene("title");
+    engine->stage->add_scene("title", title);
+    engine->stage->load_scene("title");
 
-    auto green_hill_zone = engine->stage->create_scene<game::scenes::green_hill_zone>();
+//    auto green_hill_zone = engine->stage->create_scene<game::scenes::green_hill_zone>();
 
-    engine->stage->add_scene("green_hill_zone", green_hill_zone);
-    engine->stage->load_scene("green_hill_zone");
+//    engine->stage->add_scene("green_hill_zone", green_hill_zone);
+//    engine->stage->load_scene("green_hill_zone");
 
     engine->run();
 
