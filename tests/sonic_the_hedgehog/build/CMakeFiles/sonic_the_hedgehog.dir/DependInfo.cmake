e51684d07f409fb6e294a845081d23c938849034
Victor Lopez <lvictor_9@hotmail.com>
2019-08-03 20:01:12 -0600

    Add Visual Studio Code integration

---
+++ tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/actor/actor.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/actor/actor.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/actor/animated_actor.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/actor/animated_actor.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/actor/text_actor.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/actor/text_actor.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/camera.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/camera.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/core.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/core.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/scene.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/scene.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/composition/stage.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/composition/stage.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/configuration/settings.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/configuration/settings.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/engine.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/engine.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/object/mesh.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/object/mesh.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/object/model.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/object/model.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/object/sprite.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/object/sprite.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/object/sprite_animation.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/object/sprite_animation.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/object/text.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/object/text.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/object/vertex.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/object/vertex.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/opengl/render_batch.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/opengl/render_batch.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/opengl/renderer.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/opengl/renderer.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/opengl/shader_program.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/opengl/shader_program.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/opengl/sprite_batch.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/opengl/sprite_batch.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/opengl/texture.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/opengl/texture.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/opengl/window.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/opengl/window.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/sdl/font.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/sdl/font.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/sdl/surface.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/sdl/surface.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/graphical/video.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/graphical/video.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/platform/application.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/platform/application.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/platform/exception.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/platform/exception.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/platform/filesystem.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/platform/filesystem.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/platform/input.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/platform/input.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/box.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/box.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/debug/opengl/drawer.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/debug/opengl/drawer.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/debug/opengl/shader_program.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/debug/opengl/shader_program.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/debug/opengl/vertex.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/debug/opengl/vertex.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/detail/body.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/detail/body.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/detail/character.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/detail/character.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/physics.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/physics.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/simulation/plane.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/simulation/plane.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/sound/chunk.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/sound/chunk.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/sound/mixer.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/sound/mixer.cpp.obj"
+  "C:/Source/libcrosswind/internal/src/crosswind/sound/music.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/C_/Source/libcrosswind/internal/src/crosswind/sound/music.cpp.obj"
+  "C:/Source/libcrosswind/tests/sonic_the_hedgehog/main.cpp" "C:/Source/libcrosswind/tests/sonic_the_hedgehog/build/CMakeFiles/sonic_the_hedgehog.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../../../internal/include"
+  "../../../external/CImg-1.6.0/include"
+  "../../../external/jsoncons/include"
+  "../../../external/bullet3/src"
+  "../../../platform/windows/build/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
