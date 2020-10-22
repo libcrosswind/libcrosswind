d45938b6af673beda07877fd8ab06f71c2969b28
Greentwip <vjlopezcarrillo@gmail.com>
2020-10-22 00:53:54 -0600

    Physical bodies

---
+++ internal/include/crosswind/composition/physical.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <map>
+#include <glm/glm.hpp>
+
+#include "tileson.hpp"
+
+// forward declarations
+namespace cw {
+	namespace simulation {
+		namespace detail {
+			class body;
+			class character;
+		}
+	}
+}
+
+// forward declarations
+namespace cw {
+	namespace composition {
+
+		class core;
+
+	}// namespace composition
+}// namespace cw
+
+
+
+namespace cw {
+	namespace composition {
+
+		class physical {
+			typedef std::map<std::string, std::shared_ptr<simulation::detail::body> > body_map;
+			typedef std::map<std::string, std::shared_ptr<simulation::detail::character> >  character_map;
+
+		public:
+			physical(std::shared_ptr<core> core, const std::string& name);
+			~physical();
+
+			void add_rigid_body(const std::string& body_name,
+								const glm::vec3& origin,
+								const glm::vec3& size,
+								const float& mass);
+
+			virtual std::shared_ptr<simulation::detail::body> get_rigid_body(const std::string& body_name);
+			
+			virtual void remove_rigid_body(const std::string& body_name);
+
+			virtual void add_character(const std::string& character_name,
+				const glm::vec3& origin,
+				const glm::vec2& size,
+				const float& step_height);
+
+			virtual std::shared_ptr<simulation::detail::character> get_character(const std::string& character_name);
+
+			virtual void remove_character(const std::string& character_name);
+
+		public:
+			std::string name;
+
+		private:
+			std::shared_ptr<core> core;
+		protected:
+			body_map        bodies;
+			character_map   characters;
+
+		};// class tilemap
+
+	}// namespace composition
+}// namespace cw
---
+++ internal/src/crosswind/composition/physical.cpp
@@ -0,0 +1,117 @@
+#include <filesystem>
+#include <algorithm>
+#include <vector>
+
+#include "crosswind/composition/physical.hpp"
+
+#include "crosswind/composition/core.hpp"
+
+#include "crosswind/simulation/physics.hpp"
+
+#include "crosswind/simulation/detail/body.hpp"
+
+#include "crosswind/simulation/detail/character.hpp"
+
+
+cw::composition::physical::physical(std::shared_ptr<cw::composition::core> core,
+									const std::string& name) :
+    core(core), name(name) {
+
+}
+
+cw::composition::physical::~physical()
+{
+	for (auto& body_mapping : bodies) {
+		core->physics->remove_rigid_body(body_mapping.second);
+	}
+
+	for (auto& character_mapping : characters) {
+		core->physics->remove_character(character_mapping.second);
+	}
+
+}
+
+
+void cw::composition::physical::add_rigid_body(const std::string& body_name,
+											   const glm::vec3& origin,
+											   const glm::vec3& size,
+											   const float& mass) {
+
+	if (bodies.find(body_name) == bodies.end()) {
+		bodies[body_name] = core->physics->create_primitive(simulation::physics::PRIMITIVE_PROXY::BOX,
+			origin,
+			size,
+			mass);
+
+		bodies[body_name]->physic_body->setUserPointer(this);
+
+	}
+	else {
+		throw std::runtime_error(body_name + " already exists, remove it first before adding one with the same name");
+	}
+
+}
+
+std::shared_ptr<cw::simulation::detail::body> 
+cw::composition::physical::get_rigid_body(const std::string& body_name) {
+
+	if (bodies.find(body_name) != bodies.end()) {
+		return bodies[body_name];
+	}
+	else {
+		throw std::runtime_error(body_name + " does not exist");
+	}
+
+}
+
+void cw::composition::physical::remove_rigid_body(const std::string& body_name) {
+
+	if (bodies.find(body_name) != bodies.end()) {
+		core->physics->remove_rigid_body(bodies[body_name]);
+		bodies.erase(body_name);
+	}
+	else {
+		throw std::runtime_error(body_name + " does not exist or was already removed");
+	}
+
+}
+
+
+void cw::composition::physical::add_character(const std::string& character_name,
+												const glm::vec3& origin,
+												const glm::vec2& size,
+												const float& step_height) {
+
+	if (characters.find(character_name) == characters.end()) {
+		characters[character_name] = core->physics->create_character(origin, size, step_height);
+		static_cast<btKinematicCharacterController*>(characters[character_name]->get_character_object())->
+			getGhostObject()->setUserPointer(this);
+	}
+	else {
+		throw std::runtime_error(character_name + " already exists, remove it first before adding one with the same name");
+	}
+
+}
+
+std::shared_ptr<cw::simulation::detail::character> cw::composition::physical::get_character(const std::string& character_name) {
+
+	if (characters.find(character_name) != characters.end()) {
+		return characters[character_name];
+	}
+	else {
+		throw std::runtime_error(character_name + " does not exist");
+	}
+
+}
+
+void cw::composition::physical::remove_character(const std::string& character_name) {
+
+	if (characters.find(character_name) != characters.end()) {
+		core->physics->remove_character(characters[character_name]);
+		characters.erase(character_name);
+	}
+	else {
+		throw std::runtime_error(character_name + " does not exist or was already removed");
+	}
+
+}
